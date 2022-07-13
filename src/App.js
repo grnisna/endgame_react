bc97aec394b6e5d42bb2d150beb744ccd178b30e
grnisan <mdnisanahmed63@gmail.com>
2022-07-14 02:57:01 +0600

    added banner in app js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Banner from './Components/Banner';
 import Footer from './Components/Footer';
 
 function App() {
   return (
     <div >
+      <Banner></Banner>
       <Footer />
+
       
     </div>
   );
