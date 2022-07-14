8c0562df1bcb3a88349f2870a531e8db941e479c
ShimulChandraDas <msshimul99@gmail.com>
2022-07-14 23:43:55 +0600

    add .

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Promotion from './Components/Promotion';
-import Testimonial from './Components/Testimonial';
-=======
-<<<<<<< HEAD
-import Section01 from './Components/TreeSection/Section01';
-=======
-<<<<<<< HEAD
-import Promotion from './Component/Promotion';
-import Testimonial from './Component/Testimonial';
+import Section01 from "./components/TreeSection/Section01";
 
->>>>>>> cb3ba6aa12c40d4537793fa7bdfe0e5e4dd23d36
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Section01></Section01>
-=======
       <Promotion></Promotion>
       <Testimonial></Testimonial>
-=======
->>>>>>> 817855537554b45474ec9803937029a80129b506
-import Banner from './Components/Banner';
-import Footer from './Components/Footer';
-
-function App() {
-  return (
-    <div className="App">
       <Banner></Banner>
       <Promotion></Promotion>
       <Testimonial></Testimonial>
       <Footer />
-<<<<<<< HEAD
-=======
 
-      
->>>>>>> 3839521f21e142af51d7e66808d8f45b8d71a5dc
->>>>>>> cb3ba6aa12c40d4537793fa7bdfe0e5e4dd23d36
->>>>>>> 817855537554b45474ec9803937029a80129b506
     </div>
   );
 }
