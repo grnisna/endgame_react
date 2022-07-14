9aed68a91ae79ba24019fd12741911aea191d213
grnisan <mdnisanahmed63@gmail.com>
2022-07-14 12:49:36 +0600

    checking purpose

---
+++ src/Components/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BusinessSummary = () => {
+  return (
+    <div>
+      <h1>amar soner bangla ami tomay valobasi</h1>
+    </div>
+  )
+}
+
+export default BusinessSummary
